20b108a344efcb180673bbae55bdd19d085056d7
VAM <mva-us@yandex.ru>
2023-02-02 17:56:57 +0300

    Add some code

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* ВЛОЖЕНИЕ ДВУХ ЦЕЛЫХ ЧИСЕЛ */
+/* подключаемый заголовочный файл */
+#include <stdio.h> //
+#include <iostream> // объявляет объекты cout, cin
+#include <conio.h> // необходим для работы _getch();
+using namespace std;
+int main() // основная пустая функция,тип int
+{
+	  int a, b; // объявляем целые переменные a, b
+	  setlocale(LC_ALL,"Russian"); // Функция setlocale() с аргументами
+        cout << "Введите a: "; // приглашение на ввод переменной a
+         cin >> a;
+	    cout << "Введите b: "; // приглашение на ввод переменной b
+         cin >> b;
+	    cout << "A + B = " << a + b << '\n';
+_getch();
+} 
+
